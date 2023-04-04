5bb726141d312f0ab71d3928f28e0bdf95841f39
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-04 10:27:57 +0300

    added test

--- backend/store/src/test/java/inventory/store/StoreApplicationTests.java
+++ backend/store/src/test/java/inventory/store/StoreApplicationTests.java
@@ -1,13 +1,71 @@
 package inventory.store;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class StoreApplicationTests {
+@AutoConfigureMockMvc
+class StoreControllerTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void testGetAllProducts() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/products")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isNotEmpty();
+    }
+
+    @Test
+    void testGetProductById() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/products/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).contains("product");
+    }
+
+    @Test
+    void testSaveProductDetails() throws Exception {
+        String productJson = "{\"name\":\"Test Product\",\"brand\":\"Test Brand\",\"stock\":10,\"size\":\"L\",\"price\":50.0,\"category\":\"Test Category\"}";
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isNotEmpty();
+    }
+
+    @Test
+    void testUpdateProduct() throws Exception {
+        String productJson = "{\"name\":\"Updated Product\",\"brand\":\"Updated Brand\",\"stock\":20,\"size\":\"M\",\"price\":60.0,\"category\":\"Updated Category\"}";
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/products/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString()).isNotEmpty();
+    }
+
+    @Test
+    void testDeleteProductById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/1"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
 }
